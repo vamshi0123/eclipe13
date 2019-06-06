a075c73b89a9560687c914c87e96f1124f79dff1
vamshi <vamshi.doc001@gmail.com>
2019-06-06 11:40:07 +0530

    sampie13

---
+++ src/test/java/sample13/JavaExample.java
@@ -0,0 +1,19 @@
+package sample13;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
+public class JavaExample {
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
